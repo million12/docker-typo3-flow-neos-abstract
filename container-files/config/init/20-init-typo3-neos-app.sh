462700258901adf79a7141665ecdc4f4b17de928
Marcin Ryzycki <marcin@ryzycki.com>
2015-09-05 21:53:13 +0100

    [TASK] Adjustments to changes in parent million12/nginx-php
    * Make sure Flow/Neos app init script runs *after* init scripts
      from the parent images (i.e. million12/nginx, million12/nginx-php).
    * TMPDIR from /data/tmp/php/ is now used, so it can be shared across
      containers using /data volume.
    * Seems like the tmp dir issue and new lock mechanism was causing
      failing CI tests for parts where more then one project share the
      same /data/ volume. That seems to be resolved now.

--- container-files/config/init/20-init-typo3-neos-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Call script which will initialise/configure TYPO3 app Flow/Neos when container starts.
-cd /build-typo3-app
-./configure-typo3-app.sh
---
+++ container-files/config/init/50-run-flow-neos-app.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Initialise/configure Flow/Neos app on container start.
+cd /build-typo3-app
+./configure-typo3-app.sh
